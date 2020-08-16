3686cb260bd5eba91cca4397320f389614da268b
unknown <saroamirkhanyan@gmail.com>
2020-08-16 09:23:49 +0400

    do it

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT = process.env.PORT || 8080
 const db = require('./database.json')
 const cors = require('cors')
-app.use(cors)
+app.use(cors())
 async function search(q) {
     return await customSearch.cse.list({
         auth: apiKey,
