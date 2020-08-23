17c5095d1a879ac1fdb4746b2e3883b48a2469d1
unknown <saroamirkhanyan@gmail.com>
2020-08-23 11:51:06 +0400

    first commit

--- index.js
+++ index.js
@@ -1,67 +1,49 @@
-const apiKey = 'AIzaSyDXgHfwTe8ZmOkkWetV8zfYOw-rMrepIs4';
-const searchEngineKey = '016075355924702325305:emqggy94lr4'
-const google = require('googleapis').google
-const customSearch = google.customsearch('v1')
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8080
-const db = require('./database.json')
-const cors = require('cors')
-app.use(cors())
-async function search(q) {
-    return await customSearch.cse.list({
-        auth: apiKey,
-        cx: searchEngineKey,
-        q,
-        num: 2,
-    })
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+const noLinks = ['#', 'javascript:void(0)',
+    'javascript:void(0);']
+
+db.defaults({ sites: [] })
+    .write()
+
+const sites = db.get('sites');
+
+
+async function searchSite(url) {
+    try {
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
+        const title = $('title').text()
+        const text = $('p').text()
+        const page = { url, title, text }
+        sites.push(page)
+            .write()
+        console.log(url + ' site added!')
+        url = url.slice(0, url.length - 1)
+        $('a').each((_idx, el) => {
+            let link = $(el).attr('href')
+            if (!link) return
+            if (link[0] == '#') return
+            if (link[0] == '/') link = url + link
+            if (noLinks.includes(link)) return
+            if (!link.startsWith('http')) return;
+            searchSite(link)
+        });
+    }
+    catch{
+        console.log('Page not found !')
+    }
+
+
 }
 
 
-app.get('/v1/answers', async (req, res) => {
-    const { keyword } = req.query
-    try {
-        const response = await search(keyword)
-        const answers = response.data.items.map(answer => answer.title)
-        res.json({
-            fakeData: false,
-            answers
-        })
-    }
-    catch {
-        const answers = db.filter(item => item.title.includes(keyword)).map(item => item.title).slice(0, 10)
-        res.send({
-            fakeData: true,
-            answers
-        })
-    }
-
-})
-
-app.get('/v1/search', async (req, res) => {
-    const { query } = req.query
-    try {
-        const response = await search(query)
-        const results = response.data.items.map(result => ({
-            title: result.title,
-            link: result.link,
-            description: result.snippet,
-        }))
-        res.json({
-            fakeData: false,
-            results
-        })
-    }
-    catch {
-        const results = db.filter(result => result.title.includes(query)).slice(0, 10)
-        res.send({
-            fakeData: true,
-            results
-        })
-    }
-
-})
-
-app.listen(PORT, () => console.log("SERVER STARTED ON " + PORT))
-
-
+searchSite('https://youtube.com')
